34cdd1b373dac27c5988cb8bf16a305e701a1687
predatorx7 <smushaheed@outlook.com>
2023-07-31 17:55:10 +0530

    Fix MAM transformer issues and update example

--- example_2/lib/main.dart
+++ example_2/lib/main.dart
@@ -1,69 +1,33 @@
 import 'dart:convert';
 
+import 'package:example/providers/account.dart';
 import 'package:flutter/material.dart';
-import 'intune/intune.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'intune/intune_android.dart';
 
+final rootContainer = ProviderContainer();
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   setupIntune();
-  runApp(const MyApp());
+  runApp(
+    ProviderScope(
+      parent: rootContainer,
+      child: const MyApp(),
+    ),
+  );
 }
-
-class IntuneAndroidCallbackImpl extends IntuneAndroidCallback {
-  @override
-  void onEnrollmentNotification(MAMEnrollmentStatusResult enrollmentResult) {
-    print('enrollmentResult: $enrollmentResult');
-  }
-
-  @override
-  void onUnexpectedEnrollmentNotification() {
-    print('onUnexpectedEnrollmentNotification');
-  }
-
-  @override
-  void onErrorType(MSALErrorResponse response) {
-    // TODO: implement onErrorType
-  }
-
-  @override
-  void onMsalException(MSALApiException exception) {
-    // TODO: implement onMsalException
-  }
-
-  @override
-  void onUserAuthenticationDetails(MSALUserAuthenticationDetails details) {
-    final account = details.account;
-    msalAccount = account;
-    enrMgr.registerAccountForMAM(
-      account.username,
-      account.id,
-      account.tenantId,
-      account.authority,
-    );
-  }
-
-  @override
-  void onSignOut() {
-    final a = msalAccount;
-    if (a == null) return;
-    enrMgr.unregisterAccountFromMAM(a.username, a.id);
-  }
-}
-
-MSALUserAccount? msalAccount;
-final enrMgr = AndroidEnrollmentManager();
-final app = AndroidPublicClientApplication();
 
 void setupIntune() {
   IntuneAndroid.registerWith();
+  final app = rootContainer.read(publicClientApplicationProvider);
   app
       .updateConfiguration(
         publicClientApplicationConfiguration: pcaConfig,
       )
       .then((value) => print('pca created: $value'))
       .then((value) {
-    enrMgr.setAndroidReceiver(IntuneAndroidCallbackImpl());
+    final enrMgr = rootContainer.read(enrollmentManagerProvider);
+    enrMgr.setAndroidReceiver(rootContainer.read(accountProvider.notifier));
     enrMgr
         .registerAuthentication()
         .then((value) => print('authentication registered: $value'));
@@ -86,95 +50,90 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends ConsumerStatefulWidget {
   const MyHomePage({super.key, required this.title});
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  ConsumerState<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class _MyHomePageState extends ConsumerState<MyHomePage> {
   @override
   Widget build(BuildContext context) {
+    ref.listen(snackbarMessageProvider, (p, n) {
+      if (p == n || n == null) return;
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text(
+          n,
+        ),
+      ));
+    });
+    final account = ref.watch(accountProvider);
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            const SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: () {
-                app
-                    .signIn(AcquireTokenParams(
-                  scopes: ['https://graph.microsoft.com/User.Read'],
-                  authority:
-                      'https://login.microsoftonline.com/330ab017-b6ba-4bcb-9fdd-46380820364a',
-                ))
-                    .then((value) {
-                  print('acquire token: $value');
-                }).catchError((e, s) {
-                  print(e);
-                  debugPrintStack(stackTrace: s);
-                });
-              },
-              child: const Text('Login'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                final a = msalAccount;
-                if (a == null) {
-                  return;
-                }
-                app.signOut(a.id).then((value) {
-                  print('acquire token: $value');
-                }).catchError((e, s) {
-                  print(e);
-                  debugPrintStack(stackTrace: s);
-                });
-              },
-              child: const Text('Logout'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(
-                    content: Text(
-                      json.encode(msalAccount?.encode()),
-                    ),
-                  ),
-                );
-              },
-              child: const Text('Account info'),
-            ),
-          ],
-        ),
+      body: ListView(
+        padding: const EdgeInsets.all(25),
+        children: <Widget>[
+          ElevatedButton(
+            onPressed: () {
+              final app = ref.read(publicClientApplicationProvider);
+              app
+                  .signIn(AcquireTokenParams(
+                scopes: ['https://graph.microsoft.com/User.Read'],
+                authority:
+                    'https://login.microsoftonline.com/330ab017-b6ba-4bcb-9fdd-46380820364a',
+              ))
+                  .then((value) {
+                print('acquire token: $value');
+              }).catchError((e, s) {
+                print(e);
+                debugPrintStack(stackTrace: s);
+                ref
+                    .read(snackbarMessageProvider.notifier)
+                    .msg('Failed to login $e');
+              });
+            },
+            child: const Text('Login'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              final a = ref.read(accountProvider)?.account;
+              if (a == null) {
+                return;
+              }
+              final app = ref.read(publicClientApplicationProvider);
+              app.signOut(a.id).then((value) {
+                print('acquire token: $value');
+                ref.read(snackbarMessageProvider.notifier).msg('Logged out');
+              }).catchError((e, s) {
+                print(e);
+                debugPrintStack(stackTrace: s);
+                ref
+                    .read(snackbarMessageProvider.notifier)
+                    .msg('Failed to logout $e');
+              });
+            },
+            child: const Text('Logout'),
+          ),
+          const ListTile(
+            title: Text('MAM MAMEnrollmentStatusResult'),
+          ),
+          SelectableText(
+            json.encode(ref.watch(enrollmentStatusProvider)?.encode()),
+          ),
+          const ListTile(
+            title: Text('MSAL User Auth details'),
+          ),
+          SelectableText(
+            json.encode(account?.encode()),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
