865efbbe23768a645d62747ffc7ea546b69f719e
HDFCLTD <hdfcltd@HDFCLTDs-iMac.local>
2023-10-10 12:24:16 +0530

    Remove references

--- ref/AuthCallback.java
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright 2023, Mushaheed Syed. Use of this source code is governed by a
- * BSD-style license that can be found in the LICENSE file.
- */
-
-package com.hdfc.flutter_plugins.intune_android;
-
-public class AuthCallback {}
