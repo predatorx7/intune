14d6b9090087f169977154162a1021aaf078890f
HDFCLTD <hdfcltd@HDFCLTDs-iMac.local>
2023-11-01 10:00:36 +0530

    Add unimplemented error in ios apis

--- packages/intune/ios/Classes/IntuneApiImpl.swift
+++ packages/intune/ios/Classes/IntuneApiImpl.swift
@@ -13,38 +13,46 @@ public class IntuneApiImpl: NSObject, IntuneApi {
     }
 
     func registerAuthentication(completion: @escaping (Result<Bool, Error>) -> Void) {
-        
+        completion(.error("unimplemented"))
     }
     
     func registerAccountForMAM(upn: String, aadId: String, tenantId: String, authorityURL: String, completion: @escaping (Result<Bool, Error>) -> Void) {
+        completion(.error("unimplemented"))
         
     }
     
     func unregisterAccountFromMAM(upn: String, completion: @escaping (Result<Bool, Error>) -> Void) {
+        completion(.error("unimplemented"))
         
     }
     
     func getRegisteredAccountStatus(upn: String, completion: @escaping (Result<MAMEnrollmentStatusResult, Error>) -> Void) {
+        completion(.error("unimplemented"))
         
     }
     
     func createMicrosoftPublicClientApplication(publicClientApplicationConfiguration: [String : Any?], forceCreation: Bool, enableLogs: Bool, completion: @escaping (Result<Bool, Error>) -> Void) {
+        completion(.error("unimplemented"))
         
     }
     
     func getAccounts(completion: @escaping (Result<[MSALUserAccount], Error>) -> Void) {
+        completion(.error("unimplemented"))
         
     }
     
     func acquireToken(params: AcquireTokenParams, completion: @escaping (Result<Bool, Error>) -> Void) {
+        completion(.error("unimplemented"))
         
     }
     
     func acquireTokenSilently(params: AcquireTokenSilentlyParams, completion: @escaping (Result<Bool, Error>) -> Void) {
+        completion(.error("unimplemented"))
         
     }
     
     func signOut(aadId: String?, completion: @escaping (Result<Bool, Error>) -> Void) {
+        completion(.error("unimplemented"))
         
     }
 }
