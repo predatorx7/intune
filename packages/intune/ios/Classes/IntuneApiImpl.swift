3baa0ccd9cced708714ff43c9dcd8263ccaa7fd9
HDFCLTD <hdfcltd@HDFCLTDs-iMac.local>
2023-11-01 10:15:06 +0530

    Fix error in flutter swift plugin

--- packages/intune/ios/Classes/IntuneApiImpl.swift
+++ packages/intune/ios/Classes/IntuneApiImpl.swift
@@ -6,53 +6,51 @@ import Foundation
 import Flutter
 import UIKit
 
+extension FlutterError: Error {}
+
 public class IntuneApiImpl: NSObject, IntuneApi {
+    func getUnimplementedError() -> FlutterError {
+        return FlutterError(code: "1", message: "unimplemented", details: "")
+      }
     func ping(hello: String, completion: @escaping (Result<String, Error>) -> Void) {
         completion(.success("pong \(hello)"))
         
     }
 
     func registerAuthentication(completion: @escaping (Result<Bool, Error>) -> Void) {
-        completion(.error("unimplemented"))
+        completion(.failure(getUnimplementedError()))
     }
     
     func registerAccountForMAM(upn: String, aadId: String, tenantId: String, authorityURL: String, completion: @escaping (Result<Bool, Error>) -> Void) {
-        completion(.error("unimplemented"))
-        
+        completion(.failure(getUnimplementedError()))
     }
     
     func unregisterAccountFromMAM(upn: String, completion: @escaping (Result<Bool, Error>) -> Void) {
-        completion(.error("unimplemented"))
-        
+        completion(.failure(getUnimplementedError()))
     }
     
     func getRegisteredAccountStatus(upn: String, completion: @escaping (Result<MAMEnrollmentStatusResult, Error>) -> Void) {
-        completion(.error("unimplemented"))
-        
+        completion(.failure(getUnimplementedError()))
     }
     
     func createMicrosoftPublicClientApplication(publicClientApplicationConfiguration: [String : Any?], forceCreation: Bool, enableLogs: Bool, completion: @escaping (Result<Bool, Error>) -> Void) {
-        completion(.error("unimplemented"))
-        
+        completion(.failure(getUnimplementedError()))
     }
     
     func getAccounts(completion: @escaping (Result<[MSALUserAccount], Error>) -> Void) {
-        completion(.error("unimplemented"))
-        
+        completion(.failure(getUnimplementedError()))
     }
     
     func acquireToken(params: AcquireTokenParams, completion: @escaping (Result<Bool, Error>) -> Void) {
-        completion(.error("unimplemented"))
+        completion(.failure(getUnimplementedError()))
         
     }
     
     func acquireTokenSilently(params: AcquireTokenSilentlyParams, completion: @escaping (Result<Bool, Error>) -> Void) {
-        completion(.error("unimplemented"))
-        
+        completion(.failure(getUnimplementedError()))
     }
     
     func signOut(aadId: String?, completion: @escaping (Result<Bool, Error>) -> Void) {
-        completion(.error("unimplemented"))
-        
+        completion(.failure(getUnimplementedError()))
     }
 }
